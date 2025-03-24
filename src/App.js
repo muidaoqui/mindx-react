7c26d2d90d8dcd7dec1ae507270894cae007dfd3
muidaoqui <muidao156@gmail.com>
2025-03-24 20:35:08 +0700

    import tailwindcss

--- src/App.js
+++ src/App.js
@@ -9,36 +9,45 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import UserList from './pages/js/UserList';
 import CarList from './pages/js/CarList';
 import Shopping from './pages/js/Shopping';
+// function App() {
+//   return (
+//     <Router>
+//       {/* Ẩn Navbar trên Login và Register */}
+//       {window.location.pathname !== "/login" && window.location.pathname !== "/register" && <Navbar />}
+
+//       <Routes>
+//         {/* Trang đăng ký */}
+//         <Route path="/register" element={<Register />} />
+
+//         {/* Trang đăng nhập */}
+//         <Route path="/login" element={<Login />} />
+
+//         {/* Trang chủ */}
+//         <Route path="/home" element={<Home />} />
+
+//         {/* Trang giới thiệu về Toyota */}
+//         <Route path="/about" element={<About />} />
+
+
+//         {/* Chuyển hướng mặc định */}
+//         <Route path="/" element={<Navigate to="/login" replace />} />
+//         <Route path="/user-list" element={<UserList/>} />
+//         <Route path="/car-list" element={<CarList/>} />
+//         <Route path="/shopping-cart" element={<Shopping/>} />
+//         <Route path="*" element={<h2>404 - Page Not Found</h2>} />
+
+//       </Routes>
+//     </Router>
+//   );
+// }
+
 function App() {
   return (
-    <Router>
-      {/* Ẩn Navbar trên Login và Register */}
-      {window.location.pathname !== "/login" && window.location.pathname !== "/register" && <Navbar />}
-
-      <Routes>
-        {/* Trang đăng ký */}
-        <Route path="/register" element={<Register />} />
-
-        {/* Trang đăng nhập */}
-        <Route path="/login" element={<Login />} />
-
-        {/* Trang chủ */}
-        <Route path="/home" element={<Home />} />
-
-        {/* Trang giới thiệu về Toyota */}
-        <Route path="/about" element={<About />} />
-
-
-        {/* Chuyển hướng mặc định */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/user-list" element={<UserList/>} />
-        <Route path="/car-list" element={<CarList/>} />
-        <Route path="/shopping-cart" element={<Shopping/>} />
-        <Route path="*" element={<h2>404 - Page Not Found</h2>} />
-
-      </Routes>
-    </Router>
+    <div className="text-4xl font-bold text-blue-500">
+      Hello, Tailwind CSS!
+    </div>
   );
 }
 
 export default App;
+
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Đọc tất cả file React trong src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
